3a7dbe568e4e286652fc463994078e69df89e949
Kame03 <viktor@milivojevic.in.rs>
2022-12-24 01:29:39 +0100

    Made it Multi-Threaded

--- src/main/java/rs/in/milivojevic/Main.java
+++ src/main/java/rs/in/milivojevic/Main.java
@@ -5,8 +5,10 @@ import static spark.Spark.*;
 public class Main {
     public static void main(String[] args) {
         port(8080);
-
-        threadPool(20, 10, 30000); // 20 threads, 10 min idle time, 30 seconds timeout
+        int maxThreads = 4;
+        int minThreads = 2;
+        int timeOutMillis = 30000;
+        threadPool(maxThreads, minThreads, timeOutMillis);
 
         get("/api", (request, response) -> {
             String name = request.queryParams("name");
