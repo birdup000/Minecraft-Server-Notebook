426d3a0612202e8fb5596497ea70630ab92eb6d0
Kame03 <viktor@milivojevic.in.rs>
2022-12-24 01:20:43 +0100

    Added Some Code

--- src/main/java/rs/in/milivojevic/Main.java
+++ src/main/java/rs/in/milivojevic/Main.java
@@ -1,7 +1,16 @@
 package rs.in.milivojevic;
 
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        port(8080);
+
+        threadPool(20, 10, 30000); // 20 threads, 10 min idle time, 30 seconds timeout
+
+        get("/api", (request, response) -> {
+            String name = request.queryParams("name");
+            return name + "<br>" + "request.queryParams";
+        });
     }
 }
