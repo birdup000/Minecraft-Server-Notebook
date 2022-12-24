e73bf01bf4bcceabde40e2809aeba885028de0e0
Kame03 <viktor@milivojevic.in.rs>
2022-12-24 02:46:33 +0100

    Update Main class

--- src/main/java/rs/in/milivojevic/Main.java
+++ src/main/java/rs/in/milivojevic/Main.java
@@ -1,5 +1,4 @@
 package rs.in.milivojevic;
-
 import static spark.Spark.*;
 
 public class Main {
@@ -10,9 +9,47 @@ public class Main {
         int timeOutMillis = 30000;
         threadPool(maxThreads, minThreads, timeOutMillis);
 
+        // Accept GET requests
         get("/api", (request, response) -> {
-            String name = request.queryParams("name");
-            return name + "<br>" + "request.queryParams";
+            String email = request.queryParams("email");
+            String password = request.queryParams("psw");
+            String passwordRepeat = request.queryParams("psw-repeat");
+            response.status(200);
+            if (email == null | password == null | passwordRepeat == null) {
+                response.status(206);
+                if (password != passwordRepeat) {
+                    response.status(400);
+                    return "Passwords don't match!";
+                }
+                return "Error one of the credentials aren't supplied!";
+            }
+            return email + passwordRepeat + password;
+        });
+
+        // Accept POST requests
+        post("/api", (request, response) -> {
+            String email = request.queryParams("email");
+            String password = request.queryParams("psw");
+            String passwordRepeat = request.queryParams("psw-repeat");
+            response.status(200);
+            if (email == null | password == null | passwordRepeat == null) {
+                response.status(206);
+                if (password != passwordRepeat) {
+                    response.status(400);
+                    return "Passwords don't match!";
+                }
+                return "Error one of the credentials aren't supplied!";
+            }
+            return email + passwordRepeat + password;
+        });
+
+        // Set up the CORS preflight route
+        options("/api", (request, response) -> {
+            // Enable CORS
+            response.header("Access-Control-Allow-Origin", "");
+            response.header("Access-Control-Request-Method", "");
+            response.header("Access-Control-Allow-Headers", "");
+            return "";
         });
     }
 }
